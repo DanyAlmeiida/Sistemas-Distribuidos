92db91857607af45b8bd6a84b0b1191b3cb3b906
Dany Almeida <dany_almeida@outlook.pt>
2021-11-16 01:33:04 +0000

    Refact Folder Structure and code

---
+++ src/com/estgv/server/ScriptsStorage.java
@@ -0,0 +1,19 @@
+package com.estgv.server;
+
+import com.estgv.models.Script;
+
+import java.util.HashMap;
+
+public class ScriptsStorage {
+    static HashMap<Integer, Script> scriptsList = new HashMap<>();
+
+    public static Integer add(Script script){
+        scriptsList.put(script.id, script);
+        return script.id;
+    }
+
+    public static Script get(int scriptId)
+    {
+        return scriptsList.get(scriptId);
+    }
+}
